7ae75d6904d2927f1a357c9915b981bfcd16c38a
jojoldu <jojoldu@gmail.com>
2018-04-01 19:49:55 +0900

    [AutoScaling] init.sh 변수 추가

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ec2-user/springboot-deploy
-./gradlew build
-cp /home/ec2-user/springboot-deploy/build/libs/*.jar /home/ec2-user/build/
-./scripts/deploy.sh
+PROJECT_PATH=/home/ec2-user/springboot-deploy
+$PROJECT_PATH/gradlew build
+cp $PROJECT_PATH/build/libs/*.jar /home/ec2-user/build/
+$PROJECT_PATH/scripts/deploy.sh
